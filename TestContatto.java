9c7ccc70f5de0ca1b344ef91bdb39f69814b8d1a
Kevin2133 <kevin.bellocchio.development@gmail.com>
2023-04-14 12:53:05 +0200

    Esercizio completato 14/04/23

--- TestContatto.java
+++ TestContatto.java
@@ -1,15 +1,23 @@
-import java.rmi.Remote;
 import java.util.*;
 
-import javax.sound.sampled.SourceDataLine;
-
+import javax.xml.transform.Source;
 
 
 public class TestContatto {
+
+    public static int cercaContatto (Vector<Contatto> rubrica, String cognome){
+        for(int i = 0; i < rubrica.size(); i++){
+            if(rubrica.get(i).cognome.equals(cognome)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
     
 
     public static void main (String args[]){
-        int scelta, ind, scelta2;
+        int scelta, ind, scelta1;
         Scanner scanner = new Scanner(System.in);
         Vector<Contatto> rubrica = new Vector<Contatto>();
         String nome, cognome, cellulare, email;
@@ -60,18 +68,18 @@ public class TestContatto {
                     }
                     break;
                 case 3:
-                    do{
-                        System.out.println("Inserire un indice da rimuovere");
-                        ind = scanner.nextInt();
-                        scanner.nextLine();
+                    System.out.println("Inserire il cognome del contatto da rimuovere");
+                    cognome = scanner.nextLine();
 
-                        if((ind < 0) || (ind > rubrica.size() - 1)){
-                            System.out.println("Indice non valido");
-                        }
-                    }while((ind < 0) || (ind > rubrica.size() - 1));
+                    ind = cercaContatto(rubrica, cognome);
+
+                    if(ind < 0){
+                        System.out.println("Nessun contatto trovato");
+                    }else{
+                        rubrica.remove(ind);
+                        System.out.println("Contatto rimosso con successo");
+                    }                   
                     
-                    rubrica.remove(ind);
-                    System.out.println("Contatto rimosso con successo");
                     break;
                 case 4:
                     do{
@@ -83,6 +91,65 @@ public class TestContatto {
                             System.out.println("Indice non valido");
                         }
                     }while((ind < 0) || (ind > rubrica.size() - 1));
+
+                    c = rubrica.get(ind);
+
+                    do{
+                        System.out.println("0. esci dal sottomenu");
+                        System.out.println("1. Modifica il cognome");
+                        System.out.println("2. Modifica il nome");
+                        System.out.println("3. Modifica il cellulare");
+                        System.out.println("4. Modifica email");
+    
+                        scelta1 = scanner.nextInt();
+                        scanner.nextLine();
+    
+                        switch(scelta1){
+                            case 0:
+                                break;
+                            case 1:
+                                System.out.println("Inserire il nuovo cognome");
+                                cognome = scanner.nextLine();
+
+                                c.cognome = cognome;
+
+                                rubrica.set(ind, c);
+                                System.out.println("Contatto modificato con successo");
+                                break;
+                            case 2:
+                                System.out.println("Inserisci nuovo nome");
+                                nome = scanner.nextLine();
+
+                                c.nome = nome;
+
+                                rubrica.set(ind, c);
+                                System.out.println("Contatto modificato con successo");
+                                break;
+                            case 3:
+                                System.out.println("Inserisci nuovo cellulare");
+                                cellulare = scanner.nextLine();
+
+                                c.cellulare = cellulare;
+
+                                rubrica.set(ind, c);
+                                System.out.println("Contatto modificato con successo");
+                                break;
+                            case 4:
+                                System.out.println("Inserisci nuova email");
+                                email = scanner.nextLine();
+
+                                c.email = email;
+
+                                rubrica.set(ind, c);
+                                System.out.println("Contatto modificato con successo");
+                                break;
+                            default:
+                                System.out.println("Scelta non prevista");
+                                break;
+                        }
+                    }while(scelta1 != 0);
+
+                    
                     break;
                 default:
                     System.out.println("Scelta non prevista");
